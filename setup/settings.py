071a263d4a91f9c20575813fb80cf64b9d1d2737
Rafael Fernandes <rafaelrofernandes@gmail.com>
2025-01-13 10:25:11 -0300

    Atualização e melhoria dos códigos

--- setup/settings.py
+++ setup/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
